b7f39b54c46c529895dfca37d1344883a497f0bc
MarthaNawezhi <martha.nawezhi@gmail.com>
2022-05-05 21:11:28 +0300

    rendering the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddQuoteComponent } from './quote/add-quote/add-quote.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QouteDetailComponent } from './quote/qoute-detail/qoute-detail.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: QuoteComponent,
+    children: [
+      {
+        path: 'quotes/:id',
+        component: QouteDetailComponent
+      }
+    ]
+  },
+  {
+    path: 'add-quote',
+    component: AddQuoteComponent,
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
