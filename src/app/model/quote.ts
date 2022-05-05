b18f3f53047b18497bc853e9579d2daefe262405
MarthaNawezhi <martha.nawezhi@gmail.com>
2022-05-05 22:56:34 +0300

    Auto-generated commit [ci skip]

--- src/app/model/quote.ts
+++
@@ -1,3 +0,0 @@
-export class Quote {
-    constructor(public id: number, public author: string, public quote: string,public submited_by: string, public upvotes:number = 0, public downvotes: number = 0, public created: Date,public best:boolean=false){}
-}
--- src/app/pipes/timesince.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimesincePipe } from './timesince.pipe';
-
-describe('TimesincePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimesincePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
